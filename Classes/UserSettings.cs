c1ce033a18ab261e3b4f4b0ef3c28ba43009b7f3
Vladimir Bachinsky <v.bachinsky@kralys.ch>
2022-04-15 18:32:51 +0300

    Fix

--- Classes/UserSettings.cs
+++ Classes/UserSettings.cs
@@ -6,9 +6,9 @@
         public static string SmtpServer = "smtp.gmail.com";
         public static int ImapPort = 993;
         public static bool ImapUseTls = true;
-        public static string ImapUser = "bachinsky1@gmail.com";
-        public static string ImapPassword = "qjtobltgvaqrlhad";
+        public static string ImapUser = "mail@gmail.com";
+        public static string ImapPassword = "password";
 
-        public static string SendGridKey = "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";
+        public static string SendGridKey = "";
     }
 }
